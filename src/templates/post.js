ec2a120bbaa0f77540c98e0db1cd8fb6d9e66bfd
nokeysnocheese <user@users-MacBook-Pro.local>
2021-09-27 19:03:04 -0500

    ksljf

--- src/templates/post.js
+++ src/templates/post.js
@@ -77,7 +77,7 @@ console.log(post.photoCredit)
               {/* <div style={{borderLeft:"0.3px solid #eee",borderTop:"0.1px solid #eee",borderRadius:"16px"}}> */}
             
             {/* forced update */}
-            {/* forced update */}
+            
             
             
               <table width="100%" cellpadding="0" cellspacing="0" border="0" style={{borderCollapse: "collapse"}}>
