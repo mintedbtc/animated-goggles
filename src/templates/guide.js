de063dad06c65c897663a84dfb5693e91ad9fbab
nokeysnocheese <user@users-MacBook-Pro.local>
2021-10-19 22:03:41 -0500

    askldj

--- src/templates/guide.js
+++ src/templates/guide.js
@@ -30,19 +30,22 @@ const Guide = ({ data }) => {
     shareImage: guide.image,
     guide: true,
   };
-  
+
+  console.log(guide.content)
   guide.content = guide.content.split('<img ').join('<img style="text-align:center; margin-left:auto;margin-right:auto;display:block"')
   guide.content = guide.content.split('class="ql-align-center"').join('class="ql-align-center" style="text-align:center; margin-left:auto;margin-right:auto;display:block;width:50%"')
   guide.content = guide.content.split('<em>').join('<em style="color:black">')
   guide.content = guide.content.split('a href=').join('a style="border-bottom:2px solid #307765;text-decoration:none;color:#333" href=')
-  guide.content = guide.content.split('<pre class="ql-syntax" spellcheck="false">"').join('')
-  guide.content = guide.content.split('"</pre>').join('')
+  guide.content = guide.content.split('<pre class="ql-syntax" spellcheck="false">&lt;blockquote').join('<blockquote').split('> <').join('><').split('" ').join('"').split('&gt;&lt;').join('><').split('gt').join('>').split('&>').join('>').split('";/').join('')
+  
   
   return (
     
     <Layout seo={seo}>
+      
       {breakpoints.sm ? 
       (
+        
       <div>
         
           <h2 className="uk-text-center" style={{paddingTop:"15%",marginTop:"15%"}}>{guide.title}</h2>
