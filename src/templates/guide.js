2822e522d170af9f9789c002c7732e0ca8f6ad86
nokeysnocheese <user@users-MacBook-Pro.local>
2021-10-27 22:16:45 -0500

    alskfj

--- src/templates/guide.js
+++ src/templates/guide.js
@@ -37,8 +37,8 @@ const Guide = ({ data }) => {
   guide.content = guide.content.split('<em>').join('<em style="color:black">')
   guide.content = guide.content.split('a href=').join('a style="border-bottom:2px solid #307765;text-decoration:none;color:#333" href=')
   guide.content = guide.content.split('<p>&lt;blockquote').join('<blockquote').split('> <').join('><').split('" ').join('"').split('&gt;&lt;').join('><').split('gt').join('>').split('&>').join('>').split('";/').join('')
-  guide.content = guide.content.split('<span class="ql-size-small">').join('<span style="font-weight:400; font-size:11px;">')
-  guide.content = guide.content.split('</span>').join('</span>')
+  // guide.content = guide.content.split('<span class="ql-size-small">').join('<span style="font-weight:400; font-size:11px;">')
+  // guide.content = guide.content.split('</span>').join('</span>')
   
   return (
     
