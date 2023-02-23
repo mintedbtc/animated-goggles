a0dff9a5e2e21b01a208cb2561b96514b105e6e8
mintedbtc <daniel@northwestarkansasdaily.com>
2023-02-23 10:30:39 -0600

    lkasjdf

--- src/templates/article.js
+++ src/templates/article.js
@@ -297,7 +297,7 @@ console.log(article)
             </tr>
             </table>
             {
-              article.sponsor !== null ?
+              article.sponsor !== null && article.sponsor.partnerLogo !== null && article.sponsor.partnerWebsite !== null && article.sponsor.partnerMessage !== null  && article.sponsor.website !== null && article.sponsor.signup !== null?
             (
             <div>
             <br></br>
