ef690c97044ad1470b0316dba77201fc2b3ca5cc
mintedbtc <daniel@northwestarkansasdaily.com>
2023-10-12 12:46:23 -0500

    alskjd

--- src/pages/events-in-northwest-arkansas.js
+++ src/pages/events-in-northwest-arkansas.js
@@ -242,7 +242,13 @@ render() {
                               }
                               `}
                               render={data => (
-                                <Layout seo={data.strapiHomepage.seo}>
+                                <Layout seo={
+                                  {
+                                      metaTitle: "NWA Daily Job Board",
+                                      metaDescription: "Find cool jobs in Northwest Arkansas from small businesses, startups, and corporations in Bentonville, Fayetteville, Springdale, Rogers, and more",
+                                      shareImage: data.strapiHomepage.seo.shareImage
+                                    }
+                                }>
 
 {/* const EventsPage = () => {
   const data = useStaticQuery(query);
